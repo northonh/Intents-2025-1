d0c37d6a25705db60061a99bfc2552ea919fd0cd
northonh <nobile.pedro@gmail.com>
2025-03-21 11:08:26 -0300

    Implementando ACTION_DIAL, ACTION_PICK e ACTION_CHOOSER (necessário mais de um aplicativo que trate a mesma ACTION e os mesmos metadados).

--- app/src/main/java/br/edu/ifsp/scl/bes/prdm/intents/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/bes/prdm/intents/MainActivity.kt
@@ -4,11 +4,17 @@ import android.Manifest.permission.CALL_PHONE
 import android.app.ComponentCaller
 import android.content.Intent
 import android.content.Intent.ACTION_CALL
+import android.content.Intent.ACTION_CHOOSER
+import android.content.Intent.ACTION_DIAL
+import android.content.Intent.ACTION_PICK
 import android.content.Intent.ACTION_VIEW
+import android.content.Intent.EXTRA_INTENT
+import android.content.Intent.EXTRA_TITLE
 import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
@@ -24,6 +30,7 @@ import br.edu.ifsp.scl.bes.prdm.intents.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var parameterArl: ActivityResultLauncher<Intent>
     private lateinit var cppArl: ActivityResultLauncher<String>
+    private lateinit var pickImageArl: ActivityResultLauncher<Intent>
 
     private val amb: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
@@ -59,7 +66,7 @@ class MainActivity : AppCompatActivity() {
             registerForActivityResult(ActivityResultContracts.RequestPermission()) { permissionGranted ->
                 if (permissionGranted) {
                     // Chamar o número
-                    callPhone()
+                    callPhone(true)
                 } else {
                     Toast.makeText(
                         this,
@@ -68,6 +75,13 @@ class MainActivity : AppCompatActivity() {
                     ).show()
                 }
             }
+
+        pickImageArl = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            // Recebendo URI do arquivo selecionado e abrindo em outro aplicativo para visualização (galeria)
+            if (result.resultCode == RESULT_OK) {
+                startActivity(Intent(ACTION_VIEW, result.data?.data))
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -83,16 +97,14 @@ class MainActivity : AppCompatActivity() {
             }
 
             R.id.view_mi -> {
-                val url = Uri.parse(amb.parameterTv.text.toString())
-                val browserIntent = Intent(ACTION_VIEW, url)
-                startActivity(browserIntent)
+                startActivity(browserIntent())
                 true
             }
 
             R.id.call_mi -> {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
                     if (checkSelfPermission(CALL_PHONE) == PERMISSION_GRANTED) {
-                        callPhone()
+                        callPhone(true)
                     }
                     else {
                         // Solicitar a permissão para o usuário
@@ -100,21 +112,30 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
                 else {
-                    callPhone()
+                    callPhone(true)
                 }
-
                 true
             }
 
             R.id.dial_mi -> {
+                callPhone(false)
                 true
             }
 
             R.id.pick_mi -> {
+                // Chamando um aplicativo para selecionar uma imagem do diretório público de imagens
+                val imageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).path
+                val pickImageIntent = Intent(ACTION_PICK)
+                pickImageIntent.setDataAndType(Uri.parse(imageDir), "image/*")
+                pickImageArl.launch(pickImageIntent)
                 true
             }
 
             R.id.chooser_mi -> {
+                val chooserIntent = Intent(ACTION_CHOOSER)
+                chooserIntent.putExtra(EXTRA_TITLE, "Choose your favorite navigator")
+                chooserIntent.putExtra(EXTRA_INTENT, browserIntent())
+                startActivity(chooserIntent)
                 true
             }
 
@@ -124,10 +145,15 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun callPhone() {
+    private fun callPhone(call: Boolean) {
         val number = "tel: ${amb.parameterTv.text}"
-        val callIntent = Intent(ACTION_CALL)
+        val callIntent = Intent(if (call) ACTION_CALL else ACTION_DIAL)
         callIntent.data = Uri.parse(number)
         startActivity(callIntent)
+    }
+
+    private fun browserIntent(): Intent {
+        val url = Uri.parse(amb.parameterTv.text.toString())
+        return Intent(ACTION_VIEW, url)
     }
 }
